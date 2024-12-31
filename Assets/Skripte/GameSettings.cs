d3b012439e1ea676383fce3aa12114e8fdd35e3e
Spider-Man-s <filip.posavec@fer.hr>
2024-12-31 19:02:53 +0100

    made weapon prefabs

--- Assets/Skripte/GameSettings.cs
+++ Assets/Skripte/GameSettings.cs
@@ -11,8 +11,18 @@ public static class GameSettings
         Hard
     }
 
+    public enum WeaponType
+    {
+        Pistol,
+        Revolver,
+        Shotgun,
+        Rifle
+    }
+
     public static Difficulty currentDifficulty = Difficulty.Easy;
 
     public static string Username = "";
     public static int Score = 0;
+
+    public static WeaponType ChosenWeapon = WeaponType.Pistol;
 }
